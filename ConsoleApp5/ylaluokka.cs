b941afd620e6eaa21cbd0dc2f5ea3b4fff5d9e46
k1801573 <k1801573@C-A4204-12>
2019-10-10 18:47:41 +0300

    teht

--- ConsoleApp5/ylaluokka.cs
+++ ConsoleApp5/ylaluokka.cs
@@ -6,5 +6,20 @@ namespace Projekti
 {
     class ylaluokka
     {
+        private string _tehtavanimi;
+        private int _pvm;
+
+        public ylaluokka(string tehtavanimi, int pvm)
+        {
+            _tehtavanimi = tehtavanimi;
+            _pvm = pvm;
+        }
+        public string HaeTehtavanimi()
+        {
+            return _tehtavanimi;
+        }
+
+
+
     }
 }
