a6ef51f8ab2c56af9e5d97249467218259bae136
Juffe93 <55317987+Juffe93@users.noreply.github.com>
2019-10-30 08:05:06 +0200

    Update ylaluokka.cs

--- ConsoleApp5/ylaluokka.cs
+++ ConsoleApp5/ylaluokka.cs
@@ -8,14 +8,16 @@ namespace Projekti
     {
         private string _tehtavanimi;
         private string _tehtavanSuoritusaste;
-        
+        private double _deadline;
 
 
-        public ylaluokka( string tehtavanimi, string tehtavansuoritusaste)
+
+        public ylaluokka(string tehtavanimi, string tehtavansuoritusaste, double deadline)
         {
             _tehtavanimi = tehtavanimi;
             _tehtavanSuoritusaste = tehtavansuoritusaste;
-           
+            _deadline = deadline;
+
         }
         public string HaeTehtavanimi()
         {
@@ -26,8 +28,12 @@ namespace Projekti
         {
             return _tehtavanSuoritusaste;
         }
+        public double HaeDeadline()
+        {
+            return _deadline;
+        }
 
-        
+
 
 
 
