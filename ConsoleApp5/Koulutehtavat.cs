4bcd98bdca2c69ba29484b77545eac4c6e1795ae
Juffe93 <55317987+Juffe93@users.noreply.github.com>
2019-10-31 17:18:54 +0200

    Update Koulutehtavat.cs

--- ConsoleApp5/Koulutehtavat.cs
+++ ConsoleApp5/Koulutehtavat.cs
@@ -7,7 +7,7 @@ namespace Projekti
     class koulutehtavat : ylaluokka
     {
         private string _KurssinNimi;
-        public koulutehtavat(string tehtavanimi, string suoritusaste, double deadline, string KurssinNimi) : base(tehtavanimi, suoritusaste, deadline)
+        public koulutehtavat(string tehtavanimi, string suoritusaste, string KurssinNimi, double deadline) : base(tehtavanimi, suoritusaste, deadline)
         {
             _KurssinNimi = KurssinNimi;
         }
