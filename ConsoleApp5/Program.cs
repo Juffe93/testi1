d90c0621e3c5e6d37709de25129db2653106c3d9
Juffe93 <55317987+Juffe93@users.noreply.github.com>
2019-11-08 15:35:40 +0200

    Update Program.cs

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -6,7 +6,13 @@ namespace Projekti
     {
         static void Main(string[] args)
         {
+
+
             Console.WriteLine(" - Tehtävälista - ");
+            //Console.WriteLine("1 - Koulutehtävä");
+            //Console.WriteLine("2 - Työtehtävä");
+            //Console.WriteLine("syötä tehtävänumero: ");
+            //int tehtävänumero = int.parse(console.ReadLine());
             Console.WriteLine();
             Console.WriteLine(" - Syötä Kategoria");
             Console.WriteLine(" - Tehtävä nimi");
@@ -24,6 +30,7 @@ namespace Projekti
             Console.Write("Tehtävän deadline: ");
             DateTime deadline = DateTime.Parse(Console.ReadLine());
 
+
             if (Kategoria == ("Koulutehtava"))
             {
 
@@ -35,6 +42,8 @@ namespace Projekti
                 koulutehtavat UusiKoulutehtava = new koulutehtavat(tehtavanimi, suoritusaste, kurssinNimi, deadline);
                 Console.WriteLine($"Tehtävä: { UusiKoulutehtava.HaeTehtavanimi()}. Suoritusaste: { UusiKoulutehtava.HaeSuoritusaste()}. Kurssi: {UusiKoulutehtava.HaeKurssinNimi()}. Deadline: {UusiKoulutehtava.HaeDeadline()}. ");
             }
+
+
             else
             {
 
@@ -42,13 +51,13 @@ namespace Projekti
                 String tyopiste = Console.ReadLine();
                 Console.WriteLine();
 
+
                 tyotehtavat UusiTyotehtava = new tyotehtavat(tehtavanimi, suoritusaste, tyopiste, deadline);
                 Console.WriteLine($"Tehtävä: { UusiTyotehtava.HaeTehtavanimi()}. Suoritusaste: { UusiTyotehtava.HaeSuoritusaste()}. Työpiste: { UusiTyotehtava.Haetyopiste()}.  Deadline: {UusiTyotehtava.HaeDeadline()}. ");
 
             }
 
-
-
+        
 
         }
     }
