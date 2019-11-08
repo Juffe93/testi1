6c744e8f8e60de2715e8f1a5f71392c262d3b57f
Juffe93 <55317987+Juffe93@users.noreply.github.com>
2019-11-08 15:16:56 +0200

    Update Program.cs

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -6,6 +6,15 @@ namespace Projekti
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(" - Tehtävälista - ");
+            Console.WriteLine();
+            Console.WriteLine(" - Syötä Kategoria");
+            Console.WriteLine(" - Tehtävä nimi");
+            Console.WriteLine(" - Syötä suoritustaste");
+            Console.WriteLine(" - Tehtävän deadline");
+            Console.WriteLine();
+
+
             Console.Write("Syötä Kategoria: ");
             String Kategoria = Console.ReadLine();
             Console.Write("Tehtävä nimi:");
@@ -20,6 +29,7 @@ namespace Projekti
 
                 Console.Write("kurssin nimi:");
                 String kurssinNimi = Console.ReadLine();
+                Console.WriteLine();
 
 
                 koulutehtavat UusiKoulutehtava = new koulutehtavat(tehtavanimi, suoritusaste, kurssinNimi, deadline);
@@ -30,6 +40,7 @@ namespace Projekti
 
                 Console.Write("Työpiste:");
                 String tyopiste = Console.ReadLine();
+                Console.WriteLine();
 
                 tyotehtavat UusiTyotehtava = new tyotehtavat(tehtavanimi, suoritusaste, tyopiste, deadline);
                 Console.WriteLine($"Tehtävä: { UusiTyotehtava.HaeTehtavanimi()}. Suoritusaste: { UusiTyotehtava.HaeSuoritusaste()}. Työpiste: { UusiTyotehtava.Haetyopiste()}.  Deadline: {UusiTyotehtava.HaeDeadline()}. ");
