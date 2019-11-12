8723d5d89239ed3894c4fd9b43a1e719a9796845
Juffe93 <55317987+Juffe93@users.noreply.github.com>
2019-11-12 10:03:15 +0200

    Update Program.cs

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -9,55 +9,83 @@ namespace Projekti
 
 
             Console.WriteLine(" - Tehtävälista - ");
-            //Console.WriteLine("1 - Koulutehtävä");
-            //Console.WriteLine("2 - Työtehtävä");
-            //Console.WriteLine("syötä tehtävänumero: ");
-            //int tehtävänumero = int.parse(console.ReadLine());
-            Console.WriteLine();
-            Console.WriteLine(" - Syötä Kategoria");
-            Console.WriteLine(" - Tehtävä nimi");
-            Console.WriteLine(" - Syötä suoritustaste");
-            Console.WriteLine(" - Tehtävän deadline");
+            Console.WriteLine("1 - Koulutehtävä");
+            Console.WriteLine("2 - Työtehtävä");
             Console.WriteLine();
 
 
-            Console.Write("Syötä Kategoria: ");
+
+            Console.Write("Syötä Kategoria numero: ");
             String Kategoria = Console.ReadLine();
-            Console.Write("Tehtävä nimi:");
-            String tehtavanimi = Console.ReadLine();
-            Console.Write("Syötä suoritustaste: ");
-            String suoritusaste = Console.ReadLine();
-            Console.Write("Tehtävän deadline: ");
-            DateTime deadline = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine();
 
 
-            if (Kategoria == ("Koulutehtava"))
+            if (Kategoria == ("1"))
             {
-
-                Console.Write("kurssin nimi:");
-                String kurssinNimi = Console.ReadLine();
                 Console.WriteLine();
+                Console.WriteLine(" - Tehtävän nimi");
+                Console.WriteLine(" - Kurssin nimi");
+                Console.WriteLine(" - Syötä suoritustaste");
+                Console.WriteLine(" - Tehtävän deadline");
+                Console.WriteLine();
+                Console.Write("Tehtävä nimi: ");
+                String tehtavanimi = Console.ReadLine();
+                Console.Write("kurssin nimi: ");
+                String kurssinNimi = Console.ReadLine();
+                Console.Write("Syötä suoritusaste %: ");
+                String suoritusaste = Console.ReadLine();
+                Console.Write("Tehtävän deadline: ");
+                DateTime deadline = DateTime.Parse(Console.ReadLine());
+                
+
+
 
 
                 koulutehtavat UusiKoulutehtava = new koulutehtavat(tehtavanimi, suoritusaste, kurssinNimi, deadline);
                 Console.WriteLine($"Tehtävä: { UusiKoulutehtava.HaeTehtavanimi()}. Suoritusaste: { UusiKoulutehtava.HaeSuoritusaste()}. Kurssi: {UusiKoulutehtava.HaeKurssinNimi()}. Deadline: {UusiKoulutehtava.HaeDeadline()}. ");
+                if (suoritusaste == ("100"))
+                {
+                    Console.Write("Tehävä valmis");
+                }
+                else
+                {
+                    Console.WriteLine("Tehtävä kesken");
+                }
             }
+
 
 
             else
             {
-
-                Console.Write("Työpiste:");
-                String tyopiste = Console.ReadLine();
                 Console.WriteLine();
+                Console.WriteLine(" - Tehtävän nimi");
+                Console.WriteLine(" - Työpiste");
+                Console.WriteLine(" - Syötä suoritustaste");
+                Console.WriteLine(" - Tehtävän deadline");
+                Console.WriteLine();
+                Console.Write("Tehtävä nimi: ");
+                String tehtavanimi = Console.ReadLine();
+                Console.Write("Syötä Työpiste: ");
+                String tyopiste = Console.ReadLine();
+                Console.Write("Syötä suoritusaste %: ");
+                String suoritusaste = Console.ReadLine();
+                Console.Write("Tehtävän deadline: ");
+                DateTime deadline = DateTime.Parse(Console.ReadLine());
 
 
                 tyotehtavat UusiTyotehtava = new tyotehtavat(tehtavanimi, suoritusaste, tyopiste, deadline);
                 Console.WriteLine($"Tehtävä: { UusiTyotehtava.HaeTehtavanimi()}. Suoritusaste: { UusiTyotehtava.HaeSuoritusaste()}. Työpiste: { UusiTyotehtava.Haetyopiste()}.  Deadline: {UusiTyotehtava.HaeDeadline()}. ");
-
+                if (suoritusaste == ("100"))
+                {
+                    Console.Write("Tehävä valmis");
+                }
+                else
+                {
+                    Console.Write("Tehtävä kesken");
+                }
             }
 
-        
+
 
         }
     }
